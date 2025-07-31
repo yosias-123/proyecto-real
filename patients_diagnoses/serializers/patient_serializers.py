55906b48039d9dcd1f4d205337ead37be0e905a7
Demian123Z <truenopalomino@gmail.com>
2025-07-31 13:19:34 -0500

    Patient serializer Agregando la clase buscar y el metodo de validacion

--- patients_diagnoses/serializers/patient_serializers.py
+++ patients_diagnoses/serializers/patient_serializers.py
@@ -5,4 +5,14 @@ from ..models import Patient
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        fields = '__all__'  # O lista explícita si quieres: ['id', 'name', ...]
+        fields = '__all__'  
+class SearchPatientsRequest(serializers.Serializer):
+    document_number = serializers.CharField(required=False, allow_blank=True, max_length=20)
+    name = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    sex = serializers.ChoiceField(choices=['M', 'F'], required=False)
+    region = serializers.IntegerField(required=False)
+    page = serializers.IntegerField(required=False, min_value=1)
+    per_page = serializers.IntegerField(required=False, min_value=1, max_value=100)
+
+    def validate(self, attrs):
+        return attrs
